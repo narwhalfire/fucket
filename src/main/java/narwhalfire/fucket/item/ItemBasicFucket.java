a50966f48e5007162e9c3daa7f22600b14a74bcb
NarwhalFire <narwhalfire@scottnotfound.net>
2018-07-02 01:27:20 -0400

    put a little in the click method

--- src/main/java/narwhalfire/fucket/item/ItemBasicFucket.java
+++ src/main/java/narwhalfire/fucket/item/ItemBasicFucket.java
@@ -3,7 +3,9 @@ package narwhalfire.fucket.item;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
@@ -15,14 +17,14 @@ public class ItemBasicFucket extends FucketBase {
 
 
     @Override
-    public boolean hasContainerItem(ItemStack stack) {
+    public boolean hasContainerItem(@Nonnull ItemStack stack) {
         //todo: check if there is stuff in there.
         return false;
     }
 
     @Override
     @Nonnull
-    public ItemStack getContainerItem(ItemStack itemStack) {
+    public ItemStack getContainerItem(@Nonnull ItemStack itemStack) {
         //todo: figure out what to do here
         return super.getContainerItem(itemStack);
     }
@@ -32,7 +34,22 @@ public class ItemBasicFucket extends FucketBase {
      */
     @Override
     @Nonnull
-    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+    public ActionResult<ItemStack> onItemRightClick(@Nonnull World worldIn, @Nonnull EntityPlayer playerIn, @Nonnull EnumHand handIn) {
+
+        ItemStack itemStack = playerIn.getHeldItem(handIn);
+
+        //todo: figure out what's in the fucket
+
+        RayTraceResult target = this.rayTrace(worldIn, playerIn, true);
+
+        if (target == null) {
+            return ActionResult.newResult(EnumActionResult.PASS, itemStack);
+        }
+
+
+        //todo: figure out what was clicked
+
+
         return super.onItemRightClick(worldIn, playerIn, handIn);
     }
 }
