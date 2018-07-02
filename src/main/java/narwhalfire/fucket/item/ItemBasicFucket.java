2d150cbd076db793837edd88bc83010844a05372
NarwhalFire <narwhalfire@scottnotfound.net>
2018-07-02 01:05:51 -0400

    add some methods to basic fucket
    methods yet to be implemented
    
    Signed-off-by: NarwhalFire <narwhalfire@scottnotfound.net>

--- src/main/java/narwhalfire/fucket/item/ItemBasicFucket.java
+++ src/main/java/narwhalfire/fucket/item/ItemBasicFucket.java
@@ -1,7 +1,38 @@
 package narwhalfire.fucket.item;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+
 /**
  * The Basic Fucket: Just a normal fucket capable of storing entities and whatnot.
  */
 public class ItemBasicFucket extends FucketBase {
+
+
+    @Override
+    public boolean hasContainerItem(ItemStack stack) {
+        //todo: check if there is stuff in there.
+        return false;
+    }
+
+    @Override
+    @Nonnull
+    public ItemStack getContainerItem(ItemStack itemStack) {
+        //todo: figure out what to do here
+        return super.getContainerItem(itemStack);
+    }
+
+    /**
+     * Called on equipped item right click
+     */
+    @Override
+    @Nonnull
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+        return super.onItemRightClick(worldIn, playerIn, handIn);
+    }
 }
